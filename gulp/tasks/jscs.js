611d9edd22d3e9f92ed03e7ccee00bf54486ffe6
Noam Okman <noamokman@gmail.com>
2015-09-26 13:44:43 +0300

    fixed cli and updated deps

--- gulp/tasks/jscs.js
+++ gulp/tasks/jscs.js
@@ -8,16 +8,22 @@ module.exports = function (gulp) {
 
   gulp.task('jscs:lib', function () {
     return gulp.src(config.paths.lib)
-      .pipe(jscs());
+      .pipe(jscs())
+      .pipe(jscs.reporter())
+      .pipe(jscs.reporter('fail'));
   });
 
   gulp.task('jscs:test', function () {
     return gulp.src(config.paths.test)
-      .pipe(jscs());
+      .pipe(jscs())
+      .pipe(jscs.reporter())
+      .pipe(jscs.reporter('fail'));
   });
 
   gulp.task('jscs:gulp', function () {
     return gulp.src(config.paths.gulp)
-      .pipe(jscs());
+      .pipe(jscs())
+      .pipe(jscs.reporter())
+      .pipe(jscs.reporter('fail'));
   });
 };
--- test/main.spec.js
+++ test/main.spec.js
@@ -18,54 +18,41 @@ describe('nachos-open', function () {
     });
   });
 
-  describe('invalid parameters', function () {
+  describe('without parameters', function () {
     var nachosOpen = require('../lib');
 
     it('should eventually be rejected with TypeError', function () {
-      return expect(nachosOpen()).to.eventually.be.rejectedWith(TypeError);
+      return expect(nachosOpen()).to.eventually.be.rejectedWith(TypeError, 'package name must be specified');
     });
   });
 
-  describe('open dip type', function () {
-    var nachosOpen;
+  describe('with invalid parameters', function () {
+    var nachosOpen = require('../lib');
 
-    beforeEach(function () {
-      var packagesMock = {
-        getPackage: sinon.stub().returns(Q.resolve({type: 'dip'}))
-      };
-
-      mockery.registerMock('nachos-packages', packagesMock);
-      mockery.enable({
-        useCleanCache: true,
-        warnOnReplace: false,
-        warnOnUnregistered: false
-      });
-
-      nachosOpen = require('../lib');
+    it('should reject name as function', function () {
+      return expect(nachosOpen(function () {})).to.eventually.be.rejectedWith(TypeError, 'package name must be a string');
     });
 
-    afterEach(function () {
-      mockery.deregisterMock('nachos-packages');
-      mockery.disable();
+    it('should reject name as object', function () {
+      return expect(nachosOpen({})).to.eventually.be.rejectedWith(TypeError, 'package name must be a string');
     });
 
-    it('should eventually be rejected', function () {
-      return expect(nachosOpen('test')).to.eventually.be.rejectedWith('package must be taco or burrito');
+    it('should reject name as number', function () {
+      return expect(nachosOpen(5)).to.eventually.be.rejectedWith(TypeError, 'package name must be a string');
+    });
+
+    it('should reject name as boolean', function () {
+      return expect(nachosOpen(true)).to.eventually.be.rejectedWith(TypeError, 'package name must be a string');
     });
   });
 
-  describe('open taco type', function () {
-    var nachosOpen;
-    var spawnMock;
+  describe('with valid parameters', function () {
+    describe('open dip type', function () {
+      var nachosOpen;
 
-    describe('with no main path', function () {
       beforeEach(function () {
         var packagesMock = {
-          getPackage: sinon.stub().returns(Q.resolve({
-            type: 'taco',
-            path: '',
-            config: {}
-          }))
+          getPackage: sinon.stub().returns(Q.resolve({type: 'dip'}))
         };
 
         mockery.registerMock('nachos-packages', packagesMock);
@@ -83,21 +70,107 @@ describe('nachos-open', function () {
         mockery.disable();
       });
 
-      it('should be rejected', function () {
-        return expect(nachosOpen('test')).to.eventually.be.rejectedWith('taco must have main path in nachos.json');
+      it('should eventually be rejected', function () {
+        return expect(nachosOpen('test')).to.eventually.be.rejectedWith('package must be taco or burrito');
       });
     });
 
-    describe('with main path', function () {
+    describe('open taco type', function () {
+      var nachosOpen;
+      var spawnMock;
+
+      describe('with no main path', function () {
+        beforeEach(function () {
+          var packagesMock = {
+            getPackage: sinon.stub().returns(Q.resolve({
+              type: 'taco',
+              path: '',
+              config: {}
+            }))
+          };
+
+          mockery.registerMock('nachos-packages', packagesMock);
+          mockery.enable({
+            useCleanCache: true,
+            warnOnReplace: false,
+            warnOnUnregistered: false
+          });
+
+          nachosOpen = require('../lib');
+        });
+
+        afterEach(function () {
+          mockery.deregisterMock('nachos-packages');
+          mockery.disable();
+        });
+
+        it('should be rejected', function () {
+          return expect(nachosOpen('test')).to.eventually.be.rejectedWith('taco must have main path in nachos.json');
+        });
+      });
+
+      describe('with main path', function () {
+        beforeEach(function () {
+          var packagesMock = {
+            getPackage: sinon.stub().returns(Q.resolve({
+              type: 'taco',
+              path: '',
+              config: {
+                main: 'test'
+              }
+            }))
+          };
+
+          spawnMock = sinon.stub();
+
+          var childProcessPromiseMock = {
+            spawn: spawnMock
+          };
+
+          mockery.registerMock('nachos-packages', packagesMock);
+          mockery.registerMock('child-process-promise', childProcessPromiseMock);
+          mockery.enable({
+            useCleanCache: true,
+            warnOnReplace: false,
+            warnOnUnregistered: false
+          });
+
+          nachosOpen = require('../lib');
+        });
+
+        afterEach(function () {
+          mockery.deregisterMock('nachos-packages');
+          mockery.deregisterMock('child-process-promise');
+          mockery.disable();
+        });
+
+        it('should have called spawn', function () {
+          return nachosOpen('test')
+            .then(function () {
+              return expect(spawnMock).to.have.been.calledWith('test', [], {cwd: ''});
+            });
+        });
+      });
+    });
+
+    describe('open burrito type', function () {
+      var nachosOpen;
+      var spawnMock;
+
       beforeEach(function () {
+        var getPackageMock = sinon.stub();
+
+        getPackageMock.onCall(0).returns(Q.resolve({type: 'burrito'}));
+        getPackageMock.onCall(1).returns(Q.resolve({
+          type: 'taco',
+          path: '',
+          config: {
+            main: 'test'
+          }
+        }));
+
         var packagesMock = {
-          getPackage: sinon.stub().returns(Q.resolve({
-            type: 'taco',
-            path: '',
-            config: {
-              main: 'test'
-            }
-          }))
+          getPackage: getPackageMock
         };
 
         spawnMock = sinon.stub();
@@ -123,63 +196,12 @@ describe('nachos-open', function () {
         mockery.disable();
       });
 
-      it('should have called spawn', function () {
+      it('should have asked burrito taco and call spawn', function () {
         return nachosOpen('test')
           .then(function () {
-            return expect(spawnMock).to.have.been.calledWith('test', [], {cwd: ''});
+            return expect(spawnMock).to.have.been.calledWith('test', ['test'], {cwd: ''});
           });
       });
-    });
-  });
-
-  describe('open burrito type', function () {
-    var nachosOpen;
-    var spawnMock;
-
-    beforeEach(function () {
-      var getPackageMock = sinon.stub();
-
-      getPackageMock.onCall(0).returns(Q.resolve({type: 'burrito'}));
-      getPackageMock.onCall(1).returns(Q.resolve({
-        type: 'taco',
-        path: '',
-        config: {
-          main: 'test'
-        }
-      }));
-
-      var packagesMock = {
-        getPackage: getPackageMock
-      };
-
-      spawnMock = sinon.stub();
-
-      var childProcessPromiseMock = {
-        spawn: spawnMock
-      };
-
-      mockery.registerMock('nachos-packages', packagesMock);
-      mockery.registerMock('child-process-promise', childProcessPromiseMock);
-      mockery.enable({
-        useCleanCache: true,
-        warnOnReplace: false,
-        warnOnUnregistered: false
-      });
-
-      nachosOpen = require('../lib');
-    });
-
-    afterEach(function () {
-      mockery.deregisterMock('nachos-packages');
-      mockery.deregisterMock('child-process-promise');
-      mockery.disable();
-    });
-
-    it('should have asked burrito taco and call spawn', function () {
-      return nachosOpen('test')
-        .then(function () {
-          return expect(spawnMock).to.have.been.calledWith('test', ['test'], {cwd: ''});
-        });
     });
   });
 });
